e66103e349d3563a9a712c02c5afcc348e70c468
Luiz Fernando <luiz.lfcvfernando@gmail.com>
2020-07-14 07:48:23 -0300

    Adicionando funçoes de validaçao de telefone e de email

--- js/functions.js
+++ js/functions.js
@@ -5,3 +5,18 @@ function opacidadeImageUp(id) {
 function opacidadeImageDown(id) {
   document.getElementById(id).style.opacity = 0.5;
 }
+
+function autenticarEmail() {
+  var obj = eval("document.forms[0].Email");
+  var txt = obj.value;
+  if (txt.length != 0 && (txt.indexOf("@") < 1 || txt.indexOf(".") < 7)) {
+    alert("Email incorreto");
+    obj.focus();
+  }
+}
+
+$(document).ready(function () {
+  $("#phone").mask("(00) 0.0000-0000");
+});
+
+
