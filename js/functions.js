8348c761eef0764ff93dfae5520baa2cb5796f34
Luiz Fernando <luiz.lfcvfernando@gmail.com>
2020-07-14 20:09:07 -0300

    Adicionando funçao de setar aba ativa

--- js/functions.js
+++ js/functions.js
@@ -16,7 +16,35 @@ function autenticarEmail() {
 }
 
 $(document).ready(function () {
+
   $("#phone").mask("(00) 0.0000-0000");
 });
+
+function setarAbaAtiva() {
+  var variavel = window.location.href;
+  var parametrosDaUrl = variavel.split("Site")[1];
+  
+    switch (parametrosDaUrl) {
+    case "/index.html":
+      document.getElementById("home").classList.add("active");
+      break;
+    case "/cs.html":
+      document.getElementById("cs").classList.add("active");
+      break;
+    case "/fifa.html":
+      document.getElementById("jnavbarDropdownogos").classList.add("active");
+      break;
+    case "/pes.html":
+      document.getElementById("pes").classList.add("active");
+      break;
+    case "/cadastro.html":
+      document.getElementById("cadastro").classList.add("active");
+      break;
+    case "/preco.html":
+      document.getElementById("preco").classList.add("active");
+      break;
+
+  }
+}
 
 
